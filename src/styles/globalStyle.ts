7dfcee51f80b6201a6d880e1d700d4f614e82573
DonquixoteSabo <mixboksu@gmail.com>
2022-11-24 17:21:59 +0100

    Add google fonts

--- src/styles/globalStyle.ts
+++ src/styles/globalStyle.ts
@@ -6,6 +6,9 @@ const GlobalStyle = createGlobalStyle`
   padding: 0;
   margin: 0;
 }
+html, body, a, button {
+  font-family: 'Source Sans Pro', 'Roboto', 'Arial', sans-serif;
+}
 `;
 
 export default GlobalStyle;
